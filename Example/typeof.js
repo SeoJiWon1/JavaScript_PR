16b4eb48eb84ab2fd8074a627de5d113147125cc
SeoJiWon1 <dhomp7730@gmail.com>
2023-03-20 01:45:04 +0900

    typeof

---
+++ Example/typeof.js
@@ -0,0 +1,14 @@
+// typeof 연산자 
+// type 확인 
+
+console.log(typeof 3);
+// output: "number"
+
+console.log(typeof 'Sir');
+// output: "string"
+
+console.log(typeof true);
+// output: "boolean"
+
+console.log(typeof undeclaredVariable);
+// output: "undefined"
