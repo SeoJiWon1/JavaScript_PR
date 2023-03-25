f06009aefdf85922d52bfe48aec055fa8f17f3bb
Seo_Ji Won <120435947+SeoJiWon1@users.noreply.github.com>
2023-03-25 14:30:05 +0900

    if문 예시

--- Example/if.js
+++ Example/if.js
@@ -1,13 +1,17 @@
 //if 조건문 예시
-var age = 19
-if(age > 19){
-	console.log('환영합니다');
-}
-	else{
-	console.log('나이 제한이 있습니다.');
-}
-
-
-if(document.getElementById('address').value =='') {
-  alert('아이디를 입력해주세요.');
-} 
+<script>
+    function 공인중개사합격스카우터(개론, 민법){
+        if((개론>100 || 개론<0) || (민법 >100|| 개론<0)){
+            console.log('장난치지마라');
+        }
+        else if(개론<40 || 민법 <40){
+            console.log('불합격');
+        }
+        else if(개론 + 민법 <120){
+            console.log('불합격');
+        }else{
+            console.log('합격');
+        }
+        
+    }
+</script>
